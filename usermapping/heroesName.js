7a93f90aeae8d62e6bf2e3ae7787c8ce67dd2ab6
TMP314 <tdf113ps@proton.me>
2024-01-11 11:43:33 +0200

    fix for guildData with new awakened heroes

--- usermapping/heroesName.js
+++ usermapping/heroesName.js
@@ -152,3 +152,20 @@ var heroesName = {
 	"ruler": "Gavus",
 	"disorder": "Eugene"
 }
+
+var awakanedName = {
+	"astropom": "Lyca",
+	"sq": "Safiya",
+	"divineblade": "Athalia",
+	"bsparrow": "Maetria",
+	"hfp": "Belinda",
+	"sbd": "Baden",
+	"wnt": "Solise",
+	"slk": "Brutus",
+	"windsm": "Thane",
+	"sbane": "Ezizh",
+	"sphoenix": "Talene",
+	"ruler": "Gavus",
+	"disorder": "Eugene",
+	"calypso": "Shemira"
+}
